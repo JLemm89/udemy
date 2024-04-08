b53f2070ae79eddb0171ae9b049ea180bb08fc6c
JLemm89 <166084427+JLemm89@users.noreply.github.com>
2024-04-08 11:23:55 -0700

    Add files via upload

---
+++ public/js/javadice.js
@@ -0,0 +1,19 @@
+function roll(){
+    var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+    var file1 = "./images/dice" + randomNumber1 + ".png";
+    document.querySelector(".img1").setAttribute("src", file1)
+
+    var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+    var file2 = "./images/dice" + randomNumber2 + ".png";
+    document.querySelector(".img2").setAttribute("src", file2)
+
+    if(randomNumber1 > randomNumber2){
+        document.querySelector("h1").innerHTML = "Player 1 Wins!";
+    }
+    else if(randomNumber1 < randomNumber2){
+        document.querySelector("h1").innerHTML = "Player 2 Wins!";
+    }
+    else{
+        document.querySelector("h1").innerHTML = "Draw! Roll Again."
+    }
+}
